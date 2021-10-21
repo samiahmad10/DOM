32cde2f5602ea664e710294c7c7deb5341fe2ac2
samiahmad10 <samiahmad2824@gmail.com>
2021-10-21 20:52:10 +0500

    DOM Assignment

---
+++ dom.js
@@ -0,0 +1,27 @@
+var element;
+
+element= document.getElementById("main-content")
+
+console.log(element);
+
+var element;
+
+element= document.getElementById("first-name")
+
+console.log(element);
+
+var element;
+
+element= document.getElementById("last-name")
+
+console.log(element);
+
+var element;
+
+element= document.getElementById("email")
+
+console.log(element);
+
+var element;
+
+element= document.getElementsByClassName("render") .innerHTML = "New text!";
